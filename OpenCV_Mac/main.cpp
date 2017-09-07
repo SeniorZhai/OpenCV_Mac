e6fd202b665d965769bf3eaacdea238349fdbe48
zhaitao <370985116@qq.com>
2017-09-07 18:21:37 +0800

    Initial Commit

---
+++ OpenCV_Mac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV_Mac
+//
+//  Created by zhai on 2017/9/7.
+//  Copyright © 2017年 zhai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
