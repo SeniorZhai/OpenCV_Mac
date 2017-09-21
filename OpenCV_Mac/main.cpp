ac4e1128db0c8ca1a8ea083bb57dc5f4d126ff44
zhaitao <370985116@qq.com>
2017-09-21 19:46:45 +0800

    鼠标

--- OpenCV_Mac/main.cpp
+++ OpenCV_Mac/main.cpp
@@ -1,31 +1,79 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
+using namespace std;
 using namespace cv;
 
-const int g_MaxAlphaValue = 100;
-int g_AlphaValueSlider;
-double g_dAlphaValue;
-double g_dBetaValue;
 
-Mat g_srcImage1;
-Mat g_srcImage2;
-Mat g_dstImage;
+void on_Mousehandle(int event,int x,int y,int flags,void* param);
+void DrawRectangle(Mat& img,Rect box);
+void showHelpText();
 
-void on_Trackbar(int, void *){
-    g_dAlphaValue = (double) g_AlphaValueSlider / g_MaxAlphaValue;
-    g_dBetaValue = 1 - g_dAlphaValue;
-    addWeighted(g_srcImage1, g_dAlphaValue, g_srcImage2, g_dBetaValue, 0, g_dstImage);
-    imshow("Image", g_dstImage);
-}
+Rect g_rectangle;
+bool g_bDrawingBox = false;
+RNG g_rng(12345);
+
+
 
 int main( int argc, const char** argv ){
-    g_srcImage1 = imread(argv[1]);
-    g_srcImage2 = imread(argv[2]);
-    g_AlphaValueSlider = 70;
-    namedWindow("Image",WINDOW_AUTOSIZE);
-    createTrackbar("", "Image", &g_AlphaValueSlider, g_MaxAlphaValue,on_Trackbar);
-    on_Trackbar(g_AlphaValueSlider, 0);
-    waitKey(0);
+    g_rectangle = Rect(-1,-1,0,0);
+    Mat srcImage(600,800,CV_8UC3), tempImage;
+    srcImage.copyTo(tempImage);
+    g_rectangle = Rect(-1,-1,0,0);
+    srcImage = Scalar::all(0);
+    
+    namedWindow("Window");
+    setMouseCallback("Window", on_Mousehandle, (void *) &srcImage);
+    while (1) {
+        srcImage.copyTo(tempImage);
+        if (g_bDrawingBox) {
+            DrawRectangle(tempImage, g_rectangle);
+        }
+        imshow("Window", tempImage);
+        if (waitKey(10) == 27) {
+            break;
+        }
+    }
     return 0;
+}
+
+void on_Mousehandle(int event, int x,int y,int flags,void* param) {
+    Mat& image = *(Mat *) param;
+    
+    switch (event) {
+        case EVENT_MOUSEMOVE:
+        {
+            if (g_bDrawingBox) {
+                g_rectangle.width = x - g_rectangle.x;
+                g_rectangle.height = y - g_rectangle.y;
+            }
+        }
+            break;
+        case EVENT_LBUTTONDOWN:{
+            g_bDrawingBox = true;
+            g_rectangle = Rect(x,y,0,0);
+        }
+            break;
+        case EVENT_LBUTTONUP:
+        {
+            g_bDrawingBox = false;
+            if (g_rectangle.width<0) {
+                g_rectangle.x += g_rectangle.width;
+                g_rectangle.width *= -1;
+            }
+            if (g_rectangle.height<0) {
+                g_rectangle.y += g_rectangle.height;
+                g_rectangle.height *= -1;
+                DrawRectangle(image, g_rectangle);
+            }
+        }
+            break;
+    }
+    
+}
+
+void DrawRectangle(Mat& img,Rect box){
+    // 在Image上画方
+    rectangle(img,box.tl(),box.br(),Scalar(g_rng.uniform(0,255),g_rng.uniform(0,255),g_rng.uniform(0,255)));
 }
