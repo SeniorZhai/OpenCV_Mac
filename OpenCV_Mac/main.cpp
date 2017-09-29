208ac36e53b15569431ea614378f5422271ff28e
zhaitao <370985116@qq.com>
2017-09-29 18:14:25 +0800

    add weighted

--- OpenCV_Mac/main.cpp
+++ OpenCV_Mac/main.cpp
@@ -5,22 +5,17 @@ using namespace cv;
 using namespace std;
 
 int main( int argc, const char** argv ){
-    Mat image = imread(argv[1]);
-    Mat logo = imread(argv[3]);
+    Mat image1 = imread(argv[1]);
+    Mat imgae2 = imread(argv[2]);
+    /*
+     dst = src1 * alpha + src2 * alpha + gamma
+    addWeighted(<#InputArray src1#>, <#double alpha#>, <#InputArray src2#>, double beta, <#double gamma#>, <#OutputArray dst#>)
+     */
     
-    // ROI region of interest
-    // 起点0，0 logo大小的区域
-    // Mat roi = image(Rect(100,100,logo.cols,logo.rows));
-    Mat roi = image(Range(100,100+logo.cols),Range(100,100+logo.rows));
-    
-    // 遮板 必须是灰度图
-    Mat mask = imread(argv[3],0);
-    
-    logo.copyTo(roi,mask);
-    
-    imshow("roi", image);
-    waitKey(0);
-    
+    Mat out;
+    addWeighted(image1, 0.5, imgae2, 0.5, 1, out);
+    imshow("weighted", out);
+    waitKey();
     return 0;
 }
 
