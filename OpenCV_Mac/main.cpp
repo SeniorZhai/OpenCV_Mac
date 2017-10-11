17cb3b96f85897173714a702a62cb8dcbaf577dc
zhaitao <370985116@qq.com>
2017-10-11 14:27:17 +0800

    resize

--- OpenCV_Mac/main.cpp
+++ OpenCV_Mac/main.cpp
@@ -1,44 +1,31 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc/imgproc_c.h>
 using namespace cv;
 
-
+/*
+ 图像金字塔与图片尺寸缩放
+ */
 int main( int argc, const char** argv ){
     Mat image = imread(argv[1]);
     
-    Mat element =  getStructuringElement(MORPH_RECT, Size(15,15));
+    Mat tmpImage,dstImage1,dstImage2;
+    tmpImage = image;
+    imshow("source",image);
     
-    /*
-     利用基本的膨胀和腐蚀技术，执行高级形态变化
-     第三个参数是形态学运算的类型
-     MORPH_OPEN 开运算
-     MORPH_CLOSE 闭运算
-     MORPH_GRADIENT 形态学梯度
-     MORPH_TOPHAT 顶帽
-     MORPH_BLACKHAT 黑帽
-     MORPH_ERODE 腐蚀
-     MORPH_DILATE 膨胀
-     第三个参数是形态学运算内核
-     为NULL是个3*3诶和，一般使用getStructuringElement获取
-     常用MORPH_RECT MORPH_CROSS MORPH_ELLIPSE
-     第五个参数是锚点
-     第六个参数是迭代次数
-     第七个参数边界模式
-     第八个参数是边界为常数的边界值
-     */
+    /* 第四个参数值沿水平轴的缩放系数 第五个参数是沿垂直轴的缩放系数
+       第六个参数是插值方式
+         INTER_NEAREST 最近邻插值
+         INTER_LINEAR 线性插值
+         INTER_AREA 区域插值
+         INTER_CUBIC 三次样条插值
+         INTER_LANCZOS4 Lanczos插值
+    */
+    resize(tmpImage, dstImage1, Size(tmpImage.cols/2,tmpImage.rows/2),(0,0),(0,0),INTER_LANCZOS4);
+    resize(tmpImage, dstImage2, Size(tmpImage.cols/3,tmpImage.rows/3));
     
-    morphologyEx(image, image, MORPH_GRADIENT, element);
-    
-    imshow("image", image);
-    
+    imshow("1", dstImage1);
+    imshow("2", dstImage2);
     waitKey();
-    
     return 0;
 }
-
-/*
-     膨胀 求局部最大值
-     腐蚀 求局部最小值
- */
 
