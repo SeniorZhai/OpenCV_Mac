a0924ae1acc6f8ce10b340168c783f34f0603448
zhaitao <370985116@qq.com>
2017-10-11 14:32:07 +0800

    采样缩放

--- OpenCV_Mac/main.cpp
+++ OpenCV_Mac/main.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 using namespace cv;
 
 /*
@@ -11,20 +12,13 @@ int main( int argc, const char** argv ){
     Mat tmpImage,dstImage1,dstImage2;
     tmpImage = image;
     imshow("source",image);
-    
-    /* 第四个参数值沿水平轴的缩放系数 第五个参数是沿垂直轴的缩放系数
-       第六个参数是插值方式
-         INTER_NEAREST 最近邻插值
-         INTER_LINEAR 线性插值
-         INTER_AREA 区域插值
-         INTER_CUBIC 三次样条插值
-         INTER_LANCZOS4 Lanczos插值
-    */
-    resize(tmpImage, dstImage1, Size(tmpImage.cols/2,tmpImage.rows/2),(0,0),(0,0),INTER_LANCZOS4);
-    resize(tmpImage, dstImage2, Size(tmpImage.cols/3,tmpImage.rows/3));
-    
+    // 向上采样并模糊
+    pyrUp(tmpImage, dstImage1,Size(tmpImage.cols*2,tmpImage.rows*2));
     imshow("1", dstImage1);
+    // 向下采样并模糊
+    pyrDown(tmpImage, dstImage2,Size(tmpImage.cols/2,tmpImage.rows/2));
     imshow("2", dstImage2);
+    
     waitKey();
     return 0;
 }
