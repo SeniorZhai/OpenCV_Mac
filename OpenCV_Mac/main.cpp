cd862fdb7d64d0964bf145829a90edaf6db99327
zhaitao <370985116@qq.com>
2018-07-20 16:35:57 +0800

    仿射变换

--- OpenCV_Mac/main.cpp
+++ OpenCV_Mac/main.cpp
@@ -4,22 +4,40 @@
 using namespace cv;
 
 /*
- 图像金字塔与图片尺寸缩放
+ * 放射变换
+ * getAffineTransform 获取放射矩阵
+ * getRotationMatrix2D 根据中心点旋转、放大获取放射矩阵
  */
+void warpAffineT(Mat image) {
+    Mat dst1 = Mat::zeros(image.rows, image.cols, image.type());
+    Mat dst2 = Mat::zeros(image.rows, image.cols, image.type());
+
+    Point2f srcTri[3];
+    srcTri[0] = Point2f(0,0);
+    srcTri[1] = Point2f(image.cols,0);
+    srcTri[2] = Point2f(0,image.rows);
+    Point2f dstTri[3];
+    dstTri[0] = Point2f(0,image.rows*0.3);
+    dstTri[1] = Point2f(image.cols*0.8,0);
+    dstTri[2] = Point2f(image.cols*0.1,image.rows);
+    
+    Mat warp_mat = getAffineTransform(srcTri,dstTri);
+    warpAffine(image,dst1,warp_mat,dst1.size());
+    
+    Mat rot_mat = getRotationMatrix2D(Point(dst2.cols/2, dst2.rows/2), 30.0, 0.8);
+    warpAffine(image, dst2, rot_mat, dst2.size());
+
+    imshow("Affine1", dst1);
+    imshow("Affine_2", dst2);
+}
+
 int main( int argc, const char** argv ){
     Mat image = imread(argv[1]);
     
-    Mat tmpImage,dstImage1,dstImage2;
-    tmpImage = image;
-    imshow("source",image);
-    // 向上采样并模糊
-    pyrUp(tmpImage, dstImage1,Size(tmpImage.cols*2,tmpImage.rows*2));
-    imshow("1", dstImage1);
-    // 向下采样并模糊
-    pyrDown(tmpImage, dstImage2,Size(tmpImage.cols/2,tmpImage.rows/2));
-    imshow("2", dstImage2);
+    warpAffineT(image);
     
     waitKey();
     return 0;
 }
+
 
